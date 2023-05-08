ff53c759b0ac33fb0a12b76eb387fa464fe8ed4b
Daniil Shitikov <tochka098@gmail.com>
2023-05-08 19:13:23 +0400

    fix: specify correct text.txt path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const path = require("node:path");
 
-const textFilePath = path.join(__dirname, "textt.txt");
+const textFilePath = path.join(__dirname, "text.txt");
 const stream = fs.createReadStream(textFilePath, "utf-8");
 
 let data = "";
 
-stream.on("data", (chunk) => console.log((data += chunk)));
+stream.on("data", (chunk) => console.log((data += chunk).trim()));
 stream.on("error", (error) => console.log("Error", error.message));
